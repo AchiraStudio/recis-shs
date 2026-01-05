36667e27ab536bd283ce4f90f90c8cacccfa7bcf
AchiraStudio <107109072+AchiraStudio@users.noreply.github.com>
2026-01-05 23:16:44 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/r-shs/",
+  base: '/r-shs/',
   server: {
     host: true,
     port: 5500,
     strictPort: true,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        '404': resolve(__dirname, 'index.html')  // generate 404.html
-      }
-    }
   },
 })
